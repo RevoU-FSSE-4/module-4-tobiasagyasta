789ee89e32db48707f01c5d4363a00eaa3c7fdaf
Tobias Halomoan <agyasta1808@gmail.com>
2024-04-16 20:32:53 +0700

    Configured netlify deployment, formik, and yup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
+import "./Basic";
+import Basic from "./Basic";
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>Hello world!</p>
-			</header>
-		</div>
+		<>
+			<div className='App'>
+				<header className='App-header'>
+					<p className='text-3xl text-red-600 font-bold underline'>
+						Hello world!
+					</p>
+					<Basic />
+				</header>
+			</div>
+		</>
 	);
 }
 
