da7417011edd21522e7d7fbca8add51eb51492b9
Tobias Halomoan <agyasta1808@gmail.com>
2024-04-24 21:00:31 +0700

    Working fetch in SignIn

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,6 +1,28 @@
 import { Formik, Form, Field } from "formik";
+import SingleInput from "./components/SingleInput";
+import { useEffect, useState } from "react";
 
 export const SignIn = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch!");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Formik
@@ -8,26 +30,38 @@ export const SignIn = () => {
           email: "",
           password: "",
         }}
-        onSubmit={(values) => alert(values)}
-      ></Formik>
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Form>
-            <Field type="email" name="email" placeholder="email">
-              <input type="email">Insert email here.</input>
-            </Field>
-            <Field
-              type="password"
-              name="password"
-              placeholder="password"
-            ></Field>
-          </Form>
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
+        onSubmit={(values, actions) => {
+          // setFormData(values);
+          alert(`You have submitted! \n` + JSON.stringify(values, null, 2));
+          alert(JSON.stringify(data));
+          actions.setSubmitting(false);
+          actions.resetForm({ isValidating: true });
+        }}
+      >
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <Form>
+              <Field
+                as={SingleInput}
+                type="email"
+                name="email"
+                placeholder="email"
+              ></Field>
+              <Field
+                as={SingleInput}
+                type="password"
+                name="password"
+                placeholder="password"
+              ></Field>
+              <button type="submit">Submit</button>
+            </Form>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"></div>
         </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"></div>
-      </div>
+      </Formik>
     </>
   );
 };
